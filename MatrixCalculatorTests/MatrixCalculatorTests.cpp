f866919f42b92dfd0b1d02a54c8155f34975cd31
Victor <vityadonskoy2004@mail.ru>
2026-02-19 16:54:40 +0700

    Add project files.

---
+++ MatrixCalculatorTests/MatrixCalculatorTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../MatrixCalculator/matrix.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MatrixCalculatorTests
+{
+	TEST_CLASS(MatrixCalculatorTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
